f81ac376b35c86847c0516087dacfc6a248acbcc
moons14 <moons@moons14.com>
2023-03-22 18:36:20 +0900

    feat:head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Head>
+      <title>Liber Genesis</title>
+      <meta name="description" content="Liber Genesis Front End.Visualize all conversations and impacts." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+    </Head>
+    <DefaultSeo
+      defaultTitle='Liber Genesis'
+      description='Liber Genesis Front End.Visualize all conversations and impacts.'
+      titleTemplate='%s | Liber Genesis Front End'
+    />
+    <Component {...pageProps} />
+  </>)
 }
